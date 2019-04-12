1fd3fa331b4297a5097d9ff43c75b2a4ee989cd5
Tamart <itstamart@gmail.com>
2019-04-12 12:29:51 +0200

    finished the launcher script

--- run_curriculum_multithread.py
+++ run_curriculum_multithread.py
@@ -1,5 +1,20 @@
 import os
 import datetime
+import os
+import platform
+from joblib import Parallel, delayed
+
+
+def experiment(exptype, exp_dir, seed):
+    if platform.system() == 'Windows':
+        _ = os.system(
+            'python experiments/experiments_curriculum_learning.py {0} {1}/{0}/ {2} > {1}/{0}/seed_{2}.txt'.format(
+                exptype, exp_dir, seed))  # Windows order
+    else:
+        _ = os.system(
+            'python3 experiments/experiments_curriculum_learning.py {0} {1}/{0}/ {2} > {1}/{0}/seed_{2}.txt'.format(
+                exptype, exp_dir, seed))  # Linux order
+
 
 if __name__ == '__main__':
     print('Startirng Curriculum Learning Experiments: \n'
@@ -11,4 +26,14 @@ if __name__ == '__main__':
     experiment_dir = './logs/Experiment_CurriculumLearning_{}/'.format(time)
     os.makedirs(experiment_dir, exist_ok=True)
 
-    pass
+    exp_types = ['curriculum', 'standard']
+    for exp_type in exp_types:
+        os.makedirs(experiment_dir + '/{}/'.format(exp_type), exist_ok=True)
+
+    n_seeds = 10
+
+    # TRAINING LOOP!!!
+    num_cores = 20
+    _ = Parallel(n_jobs=num_cores, verbose=5) \
+        (delayed(experiment)(exptype=exp_type, exp_dir=experiment_dir + '/{}/'.format(exp_type), seed=seed)
+         for exp_type in exp_types for seed in range(n_seeds))
